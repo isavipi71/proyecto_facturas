0fbb775c5ed048118fe793d96b6877982ad54a0c
isavipi71 <ovpin.71@gmail.com>
2024-04-10 18:44:15 +0200

    commit-toggle

--- FRONTEND/js/index.js
+++ FRONTEND/js/index.js
@@ -11,3 +11,13 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+
+function toggleMenu() {
+  var seccionIzquierda = document.getElementById("seccion_izquierda");
+  if (seccionIzquierda.style.display === "none") {
+      seccionIzquierda.style.display = "flex";
+  } else {
+      seccionIzquierda.style.display = "none";
+  }
+}
+
